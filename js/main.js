3cd961aa66a652d6d10f6aff6c908a630d11094b
apot <apotterton80@gmail.com>
2023-01-20 17:16:25 -0800

    inital

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// <-------------   Elements  --------------->
+const player = {
+    id: 1
+    pieces: 12
+    
+}
+const 
+// board
+// reset button
+// <-------------   State  --------------->
+// number of pieces for each player
+let board;
+let winner;
+let turn;
+
+// turn
+// winner
+// current position
+// check move
+// <------------- Game Logic  --------------->
+
+// 1. Initialize game
+//       board setup (8 rows, 8 columns)
+//       2 players, 12 pieces each
+//       prompt user for move
+// 2. check if move is valid
+//       players can only move forward diagonally
+//       check jump function (nested if statement)
+//       king function
+//       win logic (last person standing)
+
+// 3. icebox
+//       forfeit button
+//       track wins over multiple games
+//       "flip a coin" to pick who goes first
+//       timer
+
