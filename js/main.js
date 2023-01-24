d428f8651b7c05192b8a41edf61883633433c0ce
apot <apotterton80@gmail.com>
2023-01-24 15:22:35 -0800

    board

--- js/main.js
+++ js/main.js
@@ -8,8 +8,11 @@ const player2 = {
     id: -1,
     color: 'black'
 }
-const piece = {
-    king: false
+class Piece {
+    constructor(color) {
+        this.color = color;
+        this.king = false;
+    }
 }
 // <-------------   State  --------------->
 let winner, turn = 1, board;
@@ -17,7 +20,6 @@ let winner, turn = 1, board;
 // <------------- Game Logic  --------------->
 
 init()
-const validChoices = [...document.querySelectorAll('.valid')];
 function init() {
     board = [
         [null, null, null, null, null, null, null, null],
@@ -36,48 +38,54 @@ function checkerboard() {
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
             if ((j % 2 == 0 && i % 2 == 0) || (i % 2 !== 0 && j % 2 !== 0)) {
-                currentPiece = document.createElement('div')
-                currentPiece.style.height = '30px';
-                currentPiece.style.width = '30px';
-                currentPiece.style.borderRadius = '15px';
-                currentPiece.style.margin = '10px auto'
-                const currentSelection = document.getElementById(`r${i}c${j}`)
-                currentSelection.style.backgroundColor = 'lightgrey';
-                currentSelection.appendChild(currentPiece)
+                const currentPiece = document.createElement('div')
+                // currentPiece.style.height = '30px';
+                // currentPiece.style.width = '30px';
+                // currentPiece.style.borderRadius = '15px';
+                // currentPiece.style.margin = '10px auto'
+                const gridSquare = document.getElementById(`${i}-${j}`)
+                gridSquare.style.backgroundColor = 'lightgrey';
+                gridSquare.appendChild(currentPiece)
                 
                 if (i < 3) {
                     board[i][j] = {
                         color: player1.color,
                         king: false
                     }
-                    currentPiece.style.backgroundColor = board[i][j].color;
+                    //currentPiece.style.backgroundColor = board[i][j].color;
                 } else if (i > 4) {
                     board[i][j] = {
                         color: player2.color,
                         king: false
                     }
-                    currentPiece.style.backgroundColor = board[i][j].color;
+                    //currentPiece.style.backgroundColor = board[i][j].color;
                 }
             }
         }
     }
 }
   
-document.querySelector('div')[addEventListener('click', checkMove)]
-  
+
+
+ boardEl.addEventListener('click', checkMove)
+
+ 
 function checkMove(event) {
-    console.log(event.target)
-    
-    // if (validChoices.includes(event.target) ) {
-    //     document.querySelector('div')[addEventListener('click', function(evt) {
-    //         const newSquare = evt.target.firstChild
-    //         if (turn === 1) {
-                
-    //         } else  if (turn === -1) {
-                
-    //         }
-    //         turn *= -1;
+    const currentLocation = event.target.parentElement.id.split('-')
+    console.log('first click', currentLocation, event.target)
+    if (turn === 1){
+        if (event.target.style.backgroundColor === player1.color) {
             
-    //     })]
-    // }
+            
+            
+        }
+    }
+    else if (turn === -1){
+        if (event.target.style.backgroundColor === player2.color) {
+            console.log(turn)
+            
+            
+        return
+        }
+    }
 }
