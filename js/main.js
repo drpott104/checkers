73075962bcb7715e76c93dae7b8a7b37f26d9132
apot <apotterton80@gmail.com>
2023-01-25 10:50:19 -0800

    handleMove

--- js/main.js
+++ js/main.js
@@ -43,27 +43,26 @@ function checkerboard() {
                 const currentPiece = document.createElement('div')
                 const gridSquare = document.getElementById(`${i}-${j}`)
                 gridSquare.style.backgroundColor = 'lightgrey';
-                if (board[i][j]) {
-                    console.log('active')
-                    currentPiece.style.height = '30px';
-                    currentPiece.style.width = '30px';
-                    currentPiece.style.borderRadius = '15px';
-                    currentPiece.style.margin = '10px auto';
-                    currentPiece.style.backgroundColor = board[i][j]['color']
-                }
-                console.log(currentPiece)
                 gridSquare.appendChild(currentPiece)
                 if (i < 3) {
                     board[i][j] = {
                         color: player1.color,
                         king: false
                     }
+                    currentPiece.style.backgroundColor = board[i][j]['color']
                 } else if (i > 4) {
                     board[i][j] = {
                         color: player2.color,
                         king: false
                     }
+                    currentPiece.style.backgroundColor = board[i][j]['color']
                 }
+                
+                currentPiece.style.height = '30px';
+                currentPiece.style.width = '30px';
+                currentPiece.style.borderRadius = '15px';
+                currentPiece.style.margin = '10px auto';
+                
             }
         }
     }
@@ -74,31 +73,37 @@ function renderBoard() {
         for (let j = 0; j < 8; j++) {
             if ((j % 2 == 0 && i % 2 == 0) || (i % 2 !== 0 && j % 2 !== 0)) {
                 curSqr = document.getElementById(`${i}-${j}`).firstChild
-                if ((board[i][j])) {
+                if (board[i][j]){
+                    console.log(board[i][j]['color'])
                     curSqr.style.backgroundColor = board[i][j]['color']
+                } else {
+                    console.log('active')
+                    curSqr.style.backgroundColor = '';
                 }
             }
         }
     }
 }
-renderBoard();
 
 boardEl.addEventListener('click', function(event) {
     if (!currentLocation) {
+        console.log(event.target)
         currentLocation = event.target.parentElement.id.split('-')
-        console.log(currentLocation)
         return currentLocation;
     } else {
+        console.log(event.target)
         newLocation = event.target.parentElement.id.split('-')
-        console.log(newLocation)
         handleMove();
         return newLocation;
     }
 })
 
 function handleMove() {
-    board[newLocation[0], newLocation[1]] = board[currentLocation[0], currentLocation[1]]
-    //board[currentLocation[0], currentLocation[1]] = null;
+    
+    board[newLocation[0]][newLocation[1]] = board[currentLocation[0]][currentLocation[1]]
+    board[currentLocation[0]][currentLocation[1]] = null;
+    currentLocation = null;
+    turn *= -1;
     renderBoard();
-    console.log(board[currentLocation[0]][currentLocation[1]])
+    return board[newLocation[0]][newLocation[1]]
 }
