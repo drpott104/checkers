6028ea63e2be5c09a54d3e15a125a3d6fe88454a
apot <apotterton80@gmail.com>
2023-01-25 09:35:11 -0800

    in progress

--- js/main.js
+++ js/main.js
@@ -9,13 +9,15 @@ const player2 = {
     color: 'black'
 }
 class Piece {
-    constructor(color) {
+    constructor(color, row, col) {
         this.color = color;
         this.king = false;
+        this.row;
+        this.col;
     }
 }
 // <-------------   State  --------------->
-let winner, turn = 1, board;
+let winner, turn = 1, board, currentLocation, newLocation;
 
 // <------------- Game Logic  --------------->
 
@@ -39,53 +41,64 @@ function checkerboard() {
         for (let j = 0; j < 8; j++) {
             if ((j % 2 == 0 && i % 2 == 0) || (i % 2 !== 0 && j % 2 !== 0)) {
                 const currentPiece = document.createElement('div')
-                // currentPiece.style.height = '30px';
-                // currentPiece.style.width = '30px';
-                // currentPiece.style.borderRadius = '15px';
-                // currentPiece.style.margin = '10px auto'
                 const gridSquare = document.getElementById(`${i}-${j}`)
                 gridSquare.style.backgroundColor = 'lightgrey';
+                if (board[i][j]) {
+                    console.log('active')
+                    currentPiece.style.height = '30px';
+                    currentPiece.style.width = '30px';
+                    currentPiece.style.borderRadius = '15px';
+                    currentPiece.style.margin = '10px auto';
+                    currentPiece.style.backgroundColor = board[i][j]['color']
+                }
+                console.log(currentPiece)
                 gridSquare.appendChild(currentPiece)
-                
                 if (i < 3) {
                     board[i][j] = {
                         color: player1.color,
                         king: false
                     }
-                    //currentPiece.style.backgroundColor = board[i][j].color;
                 } else if (i > 4) {
                     board[i][j] = {
                         color: player2.color,
                         king: false
                     }
-                    //currentPiece.style.backgroundColor = board[i][j].color;
                 }
             }
         }
     }
 }
-  
 
-
- boardEl.addEventListener('click', checkMove)
-
- 
-function checkMove(event) {
-    const currentLocation = event.target.parentElement.id.split('-')
-    console.log('first click', currentLocation, event.target)
-    if (turn === 1){
-        if (event.target.style.backgroundColor === player1.color) {
-            
-            
-            
+function renderBoard() {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if ((j % 2 == 0 && i % 2 == 0) || (i % 2 !== 0 && j % 2 !== 0)) {
+                curSqr = document.getElementById(`${i}-${j}`).firstChild
+                if ((board[i][j])) {
+                    curSqr.style.backgroundColor = board[i][j]['color']
+                }
+            }
         }
     }
-    else if (turn === -1){
-        if (event.target.style.backgroundColor === player2.color) {
-            console.log(turn)
-            
-            
-        return
-        }
+}
+renderBoard();
+
+boardEl.addEventListener('click', function(event) {
+    if (!currentLocation) {
+        currentLocation = event.target.parentElement.id.split('-')
+        console.log(currentLocation)
+        return currentLocation;
+    } else {
+        newLocation = event.target.parentElement.id.split('-')
+        console.log(newLocation)
+        handleMove();
+        return newLocation;
     }
+})
+
+function handleMove() {
+    board[newLocation[0], newLocation[1]] = board[currentLocation[0], currentLocation[1]]
+    //board[currentLocation[0], currentLocation[1]] = null;
+    renderBoard();
+    console.log(board[currentLocation[0]][currentLocation[1]])
 }
